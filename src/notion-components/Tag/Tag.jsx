71e5d77514a941cce69bf122cd8c99d0918b3686
definiteconfusion <122401194+definiteconfusion@users.noreply.github.com>
2024-10-30 08:44:34 -0400

    added  class for tags

--- src/notion-components/Tag/Tag.jsx
+++ src/notion-components/Tag/Tag.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Tag = ({ title, mode, style }) => {
     return (
-        <div className={`tag-main ${mode}`} style={style} >
+        <div className={`tag-main ${mode} no-hover`} style={style} >
             <p className="tag-title">
                 {title}
             </p>
